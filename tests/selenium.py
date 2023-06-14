8801233f478152be78ca41b171099a4c2d5369d1
KirillDegtyarev <lirik19@yandex.ru>
2023-06-15 00:18:02 +0700

    new

---
+++ tests/selenium.py
@@ -0,0 +1,15 @@
+import pytest
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+@pytest.fixture(scope="function")
+def chrome_browser_instance(request):
+    options = Options()
+    options.headless = False
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    browser.close()
+
+
