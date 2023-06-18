af9f59e58fb59ec3a020f9d2f9d36185cefa8a1b
KirillDegtyarev <lirik19@yandex.ru>
2023-06-18 22:34:39 +0700

    new

--- dashboard/tests/test_selenium_dashboard.py
+++ dashboard/tests/test_selenium_dashboard.py
@@ -1,11 +1,18 @@
 import pytest
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-import time
+from django.contrib.auth.models import User
 
+# @pytest.mark.selenium
+# def test_create_new_admin_user(create_admin_user):
+#     assert create_admin_user.__str__() == 'root'
 
 @pytest.mark.selenium
-def test_dashboard_admin_login(live_server, chrome_browser_instance):
+def test_dashboard_admin_login(live_server, db_fixture_setup, chrome_browser_instance):
+
+    i = User.objects.get(id=1)
+    print(i.username)
+
     browser = chrome_browser_instance
     browser.get("%s%s" % (live_server.url, "/admin/login/"))
     
@@ -16,7 +23,7 @@ def test_dashboard_admin_login(live_server, chrome_browser_instance):
 
     user_name.send_keys("root")
     user_password.send_keys("123")
-    submit.send_keys(Keys.RETURN)
 
+    submit.send_keys(Keys.RETURN)
 
     assert "Site administration" in browser.page_source
